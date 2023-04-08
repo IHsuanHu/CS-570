978c16d1bb495261c6e0877664eac64291d3f94f
IHsuanHu <ihsuanhu@gmail.com>
2023-04-08 19:02:48 -0400

    first edit

---
+++ hw5/Treap.java
@@ -0,0 +1,16 @@
+//Name: I-Hsuan Hu
+//cwid: 20010728
+
+public class Treap {
+    public class Node<E>{
+        public E date;
+        public int priority;
+        public Node<E> left;
+        public Node<E> right;
+
+        public Node (E data, int priority) {
+            
+        }
+    }
+    
+}
