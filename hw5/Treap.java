fb752d239df22cc74ccff46c999ad351fd1c584f
IHsuanHu <ihsuanhu@gmail.com>
2023-04-09 12:33:23 -0400

    second

--- hw5/Treap.java
+++ hw5/Treap.java
@@ -1,15 +1,58 @@
+import java.util.Random;
+
 //Name: I-Hsuan Hu
 //cwid: 20010728
 
 public class Treap {
-    public class Node<E>{
-        public E date;
+    private  Random priorityGenerator;
+    private Node<E> root;
+    
+    private static class Node<E>{
+        public E data;
         public int priority;
         public Node<E> left;
         public Node<E> right;
 
         public Node (E data, int priority) {
-            
+            this.data = data;
+            this.priority = priority;
+            this.left = null;
+            this.right = null;
+        }
+        public Node (E data, int priority, Node<E> left, Node<E> right) {
+            this.data = data;
+            this.priority = priority;
+            this.left = left;
+            this.right = right;
+        }
+        public Node<E> rotationRight(Node<E> root){
+            if(root.left != null){
+                if(root.left.right != null){
+                    Node<E> temp = root.left.right;
+                    root.left.right = null;
+                    Node<E> newRoot = root.left;
+                    root.left = temp;
+                    newRoot.right = root;
+                    return newRoot;
+                }
+            }
+            System.out.println("Node not found.");
+            return null;
+        }
+        public Node<E> rotationleft(Node<E> root) {
+            if(root.right != null) {
+                if(root.right.left != null) {
+                    Node<E> temp = root.right.left;
+                    root.right.left = null;
+                    Node<E> newRoot = root.right;
+                    root.right = temp;
+                    newRoot.left = root;
+                    return newRoot;
+                }
+            }
+            System.out.println("Node not found.");
+            return null;
+
         }
     }
     
